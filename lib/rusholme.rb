ad2020dc3d93091a87321e5278dadb6bd1d175d3
Hirofumi Wakasugi <baenej@gmail.com>
2015-08-11 09:49:39 +0900

    Add Eventmachine

--- lib/rusholme.rb
+++ lib/rusholme.rb
@@ -1,5 +1,29 @@
 require "rusholme/version"
+require 'termios'
+require 'eventmachine'
 
 module Rusholme
-  # Your code goes here...
+
+  module KeyboardHandler
+
+    def convert_note_to_freq(note_number)
+      (440 * 2 ** ((note_number - 69)/12.0)).round(1)
+    end
+
+    def receive_data(buffer)
+      puts ">>> #{convert_note_to_freq(buffer.ord)}"
+    end
+  end
+
+  def self.start
+    EM.run do
+      attributes = Termios.tcgetattr($stdin).dup
+      attributes.lflag &= ~Termios::ECHO
+      attributes.lflag &= ~Termios::ICANON
+      Termios.tcsetattr($stdin, Termios::TCSANOW, attributes)
+      EM.open_keyboard(KeyboardHandler)
+    end
+  end
 end
+
+Rusholme.start
